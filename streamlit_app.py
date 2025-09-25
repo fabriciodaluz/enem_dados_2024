b309a4e725c0b3ede976dee6326264fea2cc97d6
Fabricio da Luz Pereira <104173083+fabriciodaluz@users.noreply.github.com>
2025-09-24 22:24:40 -0300

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import streamlit as st
+
+st.title("Hello Streamlit-er 👋")
+st.markdown(
+    """ 
+    This is a playground for you to try Streamlit and have fun. 
+
+    **There's :rainbow[so much] you can build!**
+    
+    We prepared a few examples for you to get started. Just 
+    click on the buttons above and discover what you can do 
+    with Streamlit. 
+    """
+)
+
+if st.button("Send balloons!"):
+    st.balloons()

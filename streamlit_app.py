866e572943d88e0277dae87178427c3e5a1ff098
Fabricio da Luz Pereira <104173083+fabriciodaluz@users.noreply.github.com>
2025-09-25 02:24:45 +0000

    new app version

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import streamlit as st
-
-st.title("Hello Streamlit-er 👋")
+st.title("Dados Enem 2024 São Paulo 📝")
 st.markdown(
     """ 
-    This is a playground for you to try Streamlit and have fun. 
+    Verifique aqui as notas da sua escola. 
 
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
     """
 )
 
-if st.button("Send balloons!"):
+if st.button("Faça a festa!"):
     st.balloons()
+dados_escolas = pd.read_csv('Análise - Tabela da lista das escolas - Detalhado.csv')
+resultados = pd.read_csv('RESULTADOS_SP_SAO_PAULO_2024.csv')
+
+dados_escolas.columns = dados_escolas.columns.str.lower().str.replace(' ', '_')
+resultados.columns = resultados.columns.str.lower().str.replace(' ', '_')
+#esultados.groupby
+
+resultados = resultados.rename(columns={'co_escola': 'código_inep'})
+resultados_escolas = pd.merge(resultados,dados_escolas, on='código_inep',how='left')
+st.dataframe(resultados)
+st.dataframe(resultados_escolas)
+
+
